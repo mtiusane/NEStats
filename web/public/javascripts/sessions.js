53b8914fe489378e4a5328a7c7b2cd2cb833cf71
Mikko Tiusanen <mtiusane@gmail.com>
2023-10-21 19:46:44 +0200

    Fixed ranking calculation Added top layout to sessions listing

--- web/public/javascripts/sessions.js
+++ web/public/javascripts/sessions.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", event => {
             fetch(`/json/game/${game_id}` ).then(data => data.json()),
             fetch(`/json/game/${game_id}/sessions/${team}/0/1024`).then(data => data.json()),
             ...excludeTeams.map(excludeTeam => fetch(`/json/game/${game_id}/sessions/${excludeTeam}/0/1024`).then(data => data.json()))
-        ]).then(([game, data, ...excludeDatas]) => {
+        ]).then(([gameData, data, ...excludeDatas]) => {
             const excludePlayers = new Set(excludeDatas.flatMap(data => data.sessions.map(s => s.player_id)));
             const sessions = data.sessions.filter(s => !excludePlayers.has(s.player_id));
             const sessionsByPlayer = Object.values(sessions.reduce((players, session) => {
@@ -24,42 +24,43 @@ document.addEventListener("DOMContentLoaded", event => {
                     sessions: []
                 }).sessions.push(session);
                 return players;
-            }, {})).map(e => {
-                // Combined session record
-                return {
-                    start: new Date(Math.min(...e.sessions.map(s => Date.parse(s.start)))).toUTCString(),
-                    end: new Date(Math.max(...e.sessions.map(s => Date.parse(s.end)))).toUTCString(),
-                    score: e.sessions.reduce((score, s) => score + s.score, 0),
-                    ping: e.sessions.reduce((ping, s) => ping + s.ping / e.sessions.length, 0),
-                    player_id: e.player_id,
-                    player_name: e.player_name,
-                    player_url: e.player_url,
-                    glicko2: e.glicko2,
-                    sessions: e.sessions,
-                };
-            }).sort((a, b) => b.glicko2.rating - a.glicko2.rating).map((e, index) => { e._index = 1 + index; return e; });
+            }, {})).map(e => { return {
+                start: new Date(Math.min(...e.sessions.map(s => Date.parse(s.start)))).toUTCString(),
+                end: new Date(Math.max(...e.sessions.map(s => Date.parse(s.end)))).toUTCString(),
+                score: e.sessions.reduce((score, s) => score + s.score, 0),
+                ping: e.sessions.reduce((ping, s) => ping + s.ping / e.sessions.length, 0),
+                player_id: e.player_id,
+                player_name: e.player_name,
+                player_url: e.player_url,
+                glicko2: e.glicko2,
+                sessions: e.sessions,
+            } }).sort((a, b) => b.glicko2.rating - a.glicko2.rating).map((e, index) => { e._index = 1 + index; return e; });
             if (team.match(/human|alien/)) {
-                sessionsByPlayer.unshift({
-                    start: new Date(Math.min(...sessionsByPlayer.map(s => Date.parse(s.start)))).toUTCString(),
-                    end: new Date(Math.max(...sessionsByPlayer.map(s => Date.parse(s.end)))).toUTCString(),
-                    score: 0, // TODO: Could show avg
-                    ping: 0, // TODO: Could show avg or range
-                    player_id: 0,
-                    player_url: '',
-                    player_name: '(team total)',
-                    glicko2: {
-                        min_range: Math.min(...sessionsByPlayer.map(s => s.glicko2.min_range)),
-                        max_range: Math.max(...sessionsByPlayer.map(s => s.glicko2.max_range)),
-                        min_rating: Math.min(...sessionsByPlayer.map(s => s.glicko2.min_rating)),
-                        max_rating: Math.max(...sessionsByPlayer.map(s => s.glicko2.max_rating)),
-                        rd: sessionsByPlayer.reduce((r,s) => r + s.glicko2.rd / sessionsByPlayer.length, 0),
-                        rating: sessionsByPlayer.reduce((r,s) => r + s.glicko2.rating / sessionsByPlayer.length, 0),
-                        volatility: sessionsByPlayer.reduce((r,s) => r + s.glicko2.volatility / sessionsByPlayer.length, 0),
-                        update_count: sessionsByPlayer.reduce((r,s) => r + s.glicko2.update_count / sessionsByPlayer.length, 0),
-                    },
-                    sessions: [],
-                    _index: "",
-                });
+                const requiredDuration = 0.6 * Date.parse(gameData.game.end) - Date.parse(gameData.game.start);
+                const ranked = sessionsByPlayer.flatMap(e => e.sessions.filter(s => (Date.parse(s.end) - Date.parse(s.start)) >= requiredDuration).map(s => e));
+                if (ranked.length > 0) {
+                    sessionsByPlayer.unshift({
+                        start: new Date(Math.min(...sessionsByPlayer.map(s => Date.parse(s.start)))).toUTCString(),
+                        end: new Date(Math.max(...sessionsByPlayer.map(s => Date.parse(s.end)))).toUTCString(),
+                        score: 0, // TODO: Could show avg
+                        ping: 0, // TODO: Could show avg or range
+                        player_id: 0,
+                        player_url: '',
+                        player_name: '(team total)',
+                        glicko2: {
+                            min_range: Math.min(...sessionsByPlayer.map(s => s.glicko2.min_range)),
+                            max_range: Math.max(...sessionsByPlayer.map(s => s.glicko2.max_range)),
+                            min_rating: Math.min(...sessionsByPlayer.map(s => s.glicko2.min_rating)),
+                            max_rating: Math.max(...sessionsByPlayer.map(s => s.glicko2.max_rating)),
+                            rd: ranked.reduce((r,s) => r + s.glicko2.rd / ranked.length, 0),
+                            rating: ranked.reduce((r,s) => r + s.glicko2.rating / ranked.length, 0),
+                            volatility: ranked.reduce((r,s) => r + s.glicko2.volatility / ranked.length, 0),
+                            update_count: ranked.reduce((r,s) => r + s.glicko2.update_count / ranked.length, 0),
+                        },
+                        sessions: [],
+                        _index: "",
+                    });
+                }
             }
             sessionsByPlayer.forEach((combinedSession, index) => {
                 const teamDisplay = {
@@ -90,8 +91,8 @@ document.addEventListener("DOMContentLoaded", event => {
                                     start: Date.parse(s.start),
                                     end: Date.parse(s.end),
                                     title: Common.formatText(teamDisplay[team].title),
-                                    color: teamDisplay[team].color, // TODO: session has no team but do we need it?
-                                }; }), { min: Date.parse(game.game.start), max: Date.parse(game.game.end) }, [ placeholder.offsetWidth, placeholder.offsetHeight ]));
+                                    color: teamDisplay[team].color,
+                                }; }), { min: Date.parse(gameData.game.start), max: Date.parse(gameData.game.end) }, [ placeholder.offsetWidth, placeholder.offsetHeight ]));
                             }, 0);
                             return placeholder;
                         });
