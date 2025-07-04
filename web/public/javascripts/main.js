cbfc251b2602e1644e91b704575a2261f5992ebf
Mikko Tiusanen <mtiusane@users.noreply.github.com>
2025-07-04 12:26:27 +0300

    Fix server names in links

--- web/public/javascripts/main.js
+++ web/public/javascripts/main.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", async event => {
-    document.querySelectorAll("#links .f__text, #menu .f__text").forEach(el => {
-        el.replaceWith(Common.createEl("span", {}, "", Common.format_text(el.text)))
-    });
+    Common.load_fields_generic("#links", {});
+    Common.load_fields_generic("#menu", {});
 });
