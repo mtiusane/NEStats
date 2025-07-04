34c5d9a4182d877f8b62ffbe22a9a4f9755ba79a
Mikko Tiusanen <mtiusane@users.noreply.github.com>
2025-07-04 13:06:14 +0300

    Remove tabs

--- web/public/javascripts/servers.js
+++ web/public/javascripts/servers.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", async event => {
     document.querySelectorAll("#content div#servers").forEach(async div => {   
         Common.scroll_table(div, (offset, limit) => {
-	    return '/json/servers/'+offset+'/'+limit;
+            return '/json/servers/'+offset+'/'+limit;
         }, data => {
-	    return data.servers;
+            return data.servers;
         }, (template, server) => {
-	    let entry = template.cloneNode(true);
+            let entry = template.cloneNode(true);
             entry.querySelector('.server').replaceWith(Common.createEl('SPAN', {}, "name text", Common.createEl('A', {
                 href: `/server/${server.id}/games`
             }, "", Common.formatText(server.displayname))));
-	    fetch(`/server/${server.id}/games/0/1`).then(r => r.json()).then(async data => {
-		entry.querySelector('.map').replaceWith(Common.createEl('SPAN', {}, "name text", data.games.length > 0 ? Common.formatText(data.games[0].map.name) : "N/A"));
-		entry.querySelector('.players').replaceWith(Common.createEl('SPAN', {}, "name text", data.games.length > 0 ? `${data.games[0].max_players} (${data.games[0].max_bots - data.games[0].max_players})` : "N/A"));
-	    });
-	    return entry;
+            fetch(`/server/${server.id}/games/0/1`).then(r => r.json()).then(async data => {
+                entry.querySelector('.map').replaceWith(Common.createEl('SPAN', {}, "name text", data.games.length > 0 ? Common.formatText(data.games[0].map.name) : "N/A"));
+                entry.querySelector('.players').replaceWith(Common.createEl('SPAN', {}, "name text", data.games.length > 0 ? `${data.games[0].max_players} (${data.games[0].max_bots - data.games[0].max_players})` : "N/A"));
+            });
+            return entry;
         });
     });
 });
