fd795349ac2524af8fb812b8cd9d1a7c15c37a15
Mikko Tiusanen <mtiusane@users.noreply.github.com>
2025-07-04 12:50:59 +0300

    Add current map information to servers listing

--- web/public/javascripts/servers.js
+++ web/public/javascripts/servers.js
@@ -9,8 +9,10 @@ document.addEventListener("DOMContentLoaded", async event => {
             entry.querySelector('.server').replaceWith(Common.createEl('SPAN', {}, "name text", Common.createEl('A', {
                 href: `/server/${server.id}/games`
             }, "", Common.formatText(server.displayname))));
-            entry.querySelector('.map').replaceWith(Common.createEl('SPAN', {}, "name text", "(TODO)"));
-            entry.querySelector('.players').replaceWith(Common.createEl('SPAN', {}, "name text", "(TODO)"));
+	    fetch(`/server/${server.id}/games/0/1`).then(r => r.json()).then(async data => {
+		entry.querySelector('.map').replaceWith(Common.createEl('SPAN', {}, "name text", data.games.length > 0 ? Common.formatText(data.games[0].map.name) : "N/A"));
+		entry.querySelector('.players').replaceWith(Common.createEl('SPAN', {}, "name text", data.games.length > 0 ? `${data.games[0].max_players} (${data.games[0].max_bots - data.games[0].max_players})` : "N/A"));
+	    });
 	    return entry;
         });
     });
